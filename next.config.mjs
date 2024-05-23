ffa48c64cf96c5cb504326de75e24aa446adf5ed
shalri <fsaonlinework777@gmail.com>
2024-05-24 07:41:45 +0800

    Fixing basePath and assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "https://github.com/shalri/fm-coding-bootcamp-testimonials-slider/",
-  assetPrefix:
-    "https://github.com/shalri/fm-coding-bootcamp-testimonials-slider/",
+  basePath: "/fm-coding-bootcamp-testimonials-slider/",
+  assetPrefix: "/fm-coding-bootcamp-testimonials-slider/",
 };
 
 export default nextConfig;

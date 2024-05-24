6576f53ce19b0b24b2b1bb0b7249aee620551e65
shalri <fsaonlinework777@gmail.com>
2024-05-24 10:17:26 +0800

    Adding assets

---
+++ components/Testimonial.tsx
@@ -0,0 +1,8 @@
+import { testimonials } from "./data";
+
+const Testimonial = () => {
+  console.log(testimonials);
+  return <div>Testimonial</div>;
+};
+
+export default Testimonial;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,28 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Primary
+        "cb-dark-blue": "hsl(240, 38%, 20%)",
+        "cb-grayish-blue": "hsl(240, 18%, 77%)",
+      },
+      fontSize: {
+        body: "32px",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      fontWeight: {
+        normal: "300",
+        semibold: "500",
+        bold: "700",
+      },
+      screens: {
+        mobile: "375px",
+        desktop: "1440px",
+      },
+    },
   },
   plugins: [],
 };
